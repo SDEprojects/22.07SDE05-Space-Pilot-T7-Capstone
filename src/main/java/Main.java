f10bb70f30f3162555faacd9eca29630207adea6
Will Park <park.wongyun@gmail.com>
2022-10-14 13:48:42 -0700

    Add intro and instructions in view AB#76

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.HashMap;
 import model.Game;
 import view.GameText;
 
