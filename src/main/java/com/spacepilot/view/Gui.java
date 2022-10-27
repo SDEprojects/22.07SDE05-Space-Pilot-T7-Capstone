9f717e61b5f6455516abaf01fb4dc96edbecd5ba
LashayC <cokleylashay@gmail.com>
2022-10-27 17:09:13 -0400

    Displaying text through Gui Window, AB#1227

--- src/main/java/com/spacepilot/view/Gui.java
+++ src/main/java/com/spacepilot/view/Gui.java
@@ -2,10 +2,8 @@ package com.spacepilot.view;
 
 import com.spacepilot.model.Music;
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -19,6 +17,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -38,6 +37,15 @@ public class Gui {
   static JSlider slider;
   static FloatControl gainControl;
   static float currentVolume;
+  static JFrame frame;
+  static JPanel inputPanel, controlPanel, statusPanel;
+  static JTextField inputTextField;
+  static JButton goBtn, menuBtn, mapBtn,repairBtn, oxygenBtn, loadBtn, unloadBtn,refuelBtn;
+  static JTextArea displayArea;
+  static JLabel shipHealthLabel,fuelLevelLabel, inventoryLabel, repairsLeftLabel, strandedAstronautsLabel;
+  static JMenu menu;
+  static JScrollPane scrollPaneDisplay;
+
   public static void main(String[] args) {
 
     new Gui();
@@ -53,34 +61,41 @@ public class Gui {
     FlowLayout flowLayout = new FlowLayout();
 
     //Creating the outermost Main Frame
-    JFrame frame = new JFrame(
+    frame = new JFrame(
         "Main Panel"); //Create Frame for content //Default layout is BorderLayout
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set closing event
 //    frame.pack(); //Sets size of frame on default
     frame.setSize(new Dimension(800, 500));
 
     //Creating the bottom panel for user input
-    JPanel inputPanel = new JPanel(); //Creates panel
-    JTextField inputTextField = new JTextField(20); //Creates input text field
+    inputPanel = new JPanel(); //Creates panel
+    inputTextField = new JTextField(20); //Creates input text field
     inputTextField.setSize(20, 5);
-    JButton goBtn = new JButton("Go"); //Creates button
+    goBtn = new JButton("Go"); //Creates button
     //Adding the Components to inputPanel using Flow Layout
     inputPanel.add(goBtn);
     inputPanel.add(inputTextField);
 
     //Creating TextArea for displaying output strings on Center-Right
-    JTextArea displayArea = new JTextArea();
+    displayArea = new JTextArea();
+    scrollPaneDisplay = new JScrollPane(displayArea); //scrollpane to let text scroll
+    displayArea.setEditable(false);//stop display from being edited
+    displayArea.setWrapStyleWord(true); //wrap at word boundaries, not characters
+    scrollPaneDisplay.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    scrollPaneDisplay.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    displayArea.setLineWrap(true);
+
 
     //Creating Panel for holding buttons on Center-Left
-    JPanel controlPanel = new JPanel();
+    controlPanel = new JPanel();
     controlPanel.setBackground(Color.blue);
-    JButton menuBtn = new JButton("Menu");
-    JButton mapBtn = new JButton("Map");
-    JButton repairBtn = new JButton("Repair");
-    JButton oxygenBtn = new JButton("Use Oxygen");
-    JButton loadBtn = new JButton("Load");
-    JButton unloadBtn = new JButton("Unload");
-    JButton refuelBtn = new JButton("Refuel");
+    menuBtn = new JButton("Menu");
+    mapBtn = new JButton("Map");
+    repairBtn = new JButton("Repair");
+    oxygenBtn = new JButton("Use Oxygen");
+    loadBtn = new JButton("Load");
+    unloadBtn = new JButton("Unload");
+    refuelBtn = new JButton("Refuel");
     controlPanel.setLayout(gridLayout); //Setting controlPanel to grid layout
     controlPanel.add(menuBtn); //Adding all buttons to control panel
     controlPanel.add(mapBtn);
@@ -91,7 +106,7 @@ public class Gui {
     controlPanel.add(refuelBtn);
 
     //Creating Top Panels for Status's
-    JPanel statusPanel = new JPanel();
+    statusPanel = new JPanel();
     GridLayout panelGridLayout = new GridLayout(3, 2, 2, 3); //Created grid layout
     statusPanel.setLayout(panelGridLayout); //Set status panel to gridLayout
     //Creating the Labels and TextAreas(for updating and displaying text)
@@ -99,18 +114,18 @@ public class Gui {
     JLabel currentPlanetLabel = new JLabel(
         "Current Planet:"); //Labels can have string names and icons.
     JTextArea currentPlanetText = new JTextArea("Pluto");
-    JLabel shipHealthLabel = new JLabel("Ship Health:");
+    shipHealthLabel = new JLabel("Ship Health:");
     JTextArea shipHealthText = new JTextArea("100");
-    JLabel fuelLevelLabel = new JLabel("Fuel Level:");
+    fuelLevelLabel = new JLabel("Fuel Level:");
     JTextArea fuelLevelText = new JTextArea("100");
-    JLabel inventoryLabel = new JLabel("Inventory:");
+    inventoryLabel = new JLabel("Inventory:");
     JTextArea inventoryText = new JTextArea("[alien baby]");
     //Right of Panel
     oxygenTimeLeftLabel = new JLabel();;
     JTextArea oxygenTimeLeftText = new JTextArea("some");
-    JLabel repairsLeftLabel = new JLabel("Repairs Left:");
+    repairsLeftLabel = new JLabel("Repairs Left:");
     JTextArea repairsLeftText = new JTextArea("2/3");
-    JLabel strandedAstronautsLabel = new JLabel("Stranded Astronauts:");
+    strandedAstronautsLabel = new JLabel("Stranded Astronauts:");
     JTextArea strandedAstronautsText = new JTextArea("2");
 
     // countdown Timer setup
@@ -208,13 +223,13 @@ public class Gui {
 
     //Attach panels to the outermost Main Frame
     frame.add(statusPanel, BorderLayout.PAGE_START);
-    frame.add(displayArea, BorderLayout.CENTER);
+    frame.add(scrollPaneDisplay, BorderLayout.CENTER);
     frame.add(controlPanel, BorderLayout.LINE_END);
     frame.add(inputPanel, BorderLayout.PAGE_END);
     frame.add(musicPanel, BorderLayout.CENTER);
 
     //Creating a menu
-    JMenu menu = new JMenu("Menu");
+    menu = new JMenu("Menu");
       //Creating menu items
     JMenuItem quit = new JMenuItem("Quit");
     JMenuItem saveQuit = new JMenuItem("Save and Quit");
@@ -259,6 +274,12 @@ public class Gui {
 
   public static void playMusic() {
     Music.playAudioMusic("Space_Chill.wav");
+  }
+  //Helps convert sout to displayTextArea
+  public void appendText(String text){
+    displayArea.append(text);
+    displayArea.setCaretPosition((displayArea.getDocument().getLength()));
+//    displayArea.update(displayArea.getGraphics());
   }
 
 }
