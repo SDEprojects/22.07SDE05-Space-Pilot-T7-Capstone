8eebb7ce7c7a97504a384d61b084fa25b43454f6
blworth <108500095+blworth@users.noreply.github.com>
2022-11-04 16:06:15 -0500

    fuel resets on godmode, AB#1446

--- src/main/java/com/spacepilot/controller/Controller.java
+++ src/main/java/com/spacepilot/controller/Controller.java
@@ -182,7 +182,7 @@ public class Controller {
     gui.getFuelLevelBar().setValue(spacecraft.getFuel());
     gui.getFuelLevelBar().setString("Fuel: " + spacecraft.getFuel() + "%");
     gui.getShipHealthBar().setValue(spacecraft.getHealth());
-    gui.getShipHealthBar().setString("Health: " +  spacecraft.getHealth() + "%");
+    gui.getShipHealthBar().setString("Health: " + spacecraft.getHealth() + "%");
     //needs to connect health and fuel to spacecraft model
 
     //Reset timer to 3 minutes
@@ -309,6 +309,9 @@ public class Controller {
           String preReq = destinationPlanet.getPreReq();
 
           spacecraft.setCurrentPlanet(returnPlanet(command[1]));
+          if (!command[1].equals("orbit")) {
+            updateFuel();
+          }
           if (command[1].equals("orbit") && (!healthTimerBoolean || !alienTimerBoolean)) {
             if (!healthTimerBoolean) {
               healthTimer.stop();
@@ -543,7 +546,6 @@ public class Controller {
     gui.getShipHealthBar().setValue(game.getSpacecraft().getHealth());
     gui.getShipHealthBar().setString("Health: " + 100 + "%");
 
-
     gui.getTicktock().setMinutes(3);
     gui.getTicktock().setSeconds(1);
   }
@@ -555,10 +557,10 @@ public class Controller {
         >= (double) 5 / 5;
 
     if (!userWon) {
-      if(game.getSpacecraft().getFuel() < 1) {
+      if (game.getSpacecraft().getFuel() < 1) {
 //        game.setOver(true);
         gui.showGameOverLoseScreen();
-      } else if(gui.getShipHealthBar().getValue() < 1 ){
+      } else if (gui.getShipHealthBar().getValue() < 1) {
         gui.showGameOverLoseScreen();
         if (!healthTimerBoolean) {
           healthTimer.stop();
@@ -566,10 +568,10 @@ public class Controller {
           alienTimer.stop();
         }
 
-      } else if (gui.getTicktock().getOxygenTickerLose()){
+      } else if (gui.getTicktock().getOxygenTickerLose()) {
         gui.showGameOverLoseScreen();
 
-      } else{
+      } else {
         return;
       }
     } else if (userWon) {
@@ -583,15 +585,15 @@ public class Controller {
       @Override
       public void actionPerformed(ActionEvent e) {
         gui.getShipHealthBar().setValue((spacecraft.getHealth() - 1));
-        spacecraft.setHealth( (spacecraft.getHealth() - 1));
+        spacecraft.setHealth((spacecraft.getHealth() - 1));
         gui.getShipHealthBar().setString("Health: " + spacecraft.getHealth() + "%");
         gui.warningMessage();
-        if(spacecraft.getHealth() < 1) {
+        if (spacecraft.getHealth() < 1) {
           healthTimer.stop();
           gui.getShipHealthBar().setValue(0);
           gui.getShipHealthBar().setString("Health: " + 0 + "%");
           spacecraft.setHealth(0);
-         checkGameResult();
+          checkGameResult();
         }
       }
     });
@@ -608,10 +610,10 @@ public class Controller {
     });
   }
 
-public void updateFuel(){
-  Spacecraft spacecraft = game.getSpacecraft();
-  gui.getFuelLevelBar().setValue((spacecraft.getFuel() - 10));
-  spacecraft.setFuel((spacecraft.getFuel() - 10));
-  gui.getFuelLevelBar().setString("Fuel: " + spacecraft.getFuel() + "%");
-}
+  public void updateFuel() {
+    Spacecraft spacecraft = game.getSpacecraft();
+    gui.getFuelLevelBar().setValue((spacecraft.getFuel() - 10));
+    spacecraft.setFuel((spacecraft.getFuel() - 10));
+    gui.getFuelLevelBar().setString("Fuel: " + spacecraft.getFuel() + "%");
+  }
 }
